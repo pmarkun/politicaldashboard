001a6335d0d019e15392cea6ba7fd5eaaec59ad0
Pedro Markun <pedro@markun.com.br>
2018-08-23 19:11:44 -0300

    first

---
+++ dashboard/admin.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+from dashboard.models import Cidade, Colaborador, Responsavel
+
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+
+# Register your models here.
+
+class CidadeResource(resources.ModelResource):
+
+    class Meta():
+        model = Cidade
+        fields = ('id', 'ibge', 'nome', 'ra', 'rg','pop', 'votos',)
+
+class ColaboradorResource(resources.ModelResource):
+
+    class Meta():
+        model = Colaborador
+        fields = ('id', 'nome', 'tipo', 'obs')
+
+class CidadeAdmin(ImportExportModelAdmin):
+    def lista_colaboradores(self,obj):
+        return ', '.join([c.nome + " ("+c.tipo+")" for c in obj.colaboradores.all()])
+
+    lista_colaboradores.short_description = 'Colaboradores'
+
+    search_fields = ('nome',)
+    list_display = ('nome', 'pop', 'votos', 'prioridade', 'situacao_digital', 'situacao_territorio', 'lista_colaboradores')
+    list_editable = ('situacao_digital','situacao_territorio')
+    list_filter = ('prioridade', 'rg')
+    filter_horizontal = ('colaboradores',)
+
+    resource_class = CidadeResource
+
+
+class ColaboradorAdmin(ImportExportModelAdmin):
+    def lista_cidades(self,obj):
+        return [c for c in obj.colaboradores_cidade.all()]
+
+    lista_cidades.short_description = 'Territórios'
+
+    search_fields = ('nome',)
+    list_filter = ('tipo','colaboradores_cidade')
+    list_display = ('nome', 'tel', 'tipo', 'lista_cidades')
+
+    resource_class = ColaboradorResource
+
+admin.site.site_header = 'ZéGustavo2018/2019 Dashboard'
+admin.site.register(Cidade, CidadeAdmin)
+admin.site.register(Colaborador, ColaboradorAdmin)
+admin.site.register(Responsavel)
---
+++ dashboard/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+
+SITUACAO_CHOICES = (
+    (-2, 'emergencia!'),
+    (-1, 'ruim'),
+    (0, 'regular'),
+    (1, 'bom'),
+    (2, 'ótimo'),
+)
+
+PRIORIDADE_CHOICES = (
+    ('BAIXA', 'Baixa'),
+    ('MEDIA', 'Média'),
+    ('ALTA', 'Alta'),
+    ('ALTISSIMA', 'Altissima')
+)
+
+COLABORADOR_CHOICES = (
+    ('CODEPUTADO', 'CoDeputado'),
+    ('DOBRADA', 'Dobrada'),
+    ('AGENTE', 'Agente Público'),
+    ('MOBILIZADOR', 'Mobilizador'),
+)
+
+# Create your models here.
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=200)
+    tel = models.CharField(max_length=200, blank=True)
+    email = models.EmailField(blank=True)
+    tipo = models.CharField(max_length=10, choices=COLABORADOR_CHOICES,default=None)
+    prioridade = models.CharField(max_length=10, choices=PRIORIDADE_CHOICES, default=0)
+    obs = models.TextField(blank=True)
+    foto = models.ImageField(upload_to='colaboradores', blank=True)
+    facebook = models.URLField(blank=True)
+    instagram = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.nome
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=200)
+    ibge = models.CharField(max_length=200, blank=True)
+    pop = models.IntegerField(default=0)
+    ra = models.CharField(max_length=200, blank=True)
+    rg = models.CharField(max_length=200, blank=True)
+    votos = models.IntegerField(default=0)
+    votos_marina = models.IntegerField(default=0)
+    prioridade = models.CharField(max_length=10, choices=PRIORIDADE_CHOICES, default='BAIXA')
+    situacao_digital = models.IntegerField(choices=SITUACAO_CHOICES,default=0)
+    situacao_territorio = models.IntegerField(choices=SITUACAO_CHOICES,default=0)
+    colaboradores = models.ManyToManyField(Colaborador, related_name='colaboradores_cidade')
+
+    def __str__(self):
+        return self.nome
+
+class Responsavel(models.Model):
+    email = models.EmailField()
+    nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
